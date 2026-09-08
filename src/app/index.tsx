aea4e24ab86430cdfb6f45266919af3c122eba4e
Ashley <ashleymonyela42@gmail.com>
2026-09-08 13:26:28 +0200

    initial commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -429,7 +429,7 @@ export default function App() {
   // DELETE MENU ITEM
   // -----------------------------------------------------
 
-  const deleteMenuItem = (id) => {
+  const deleteMenuItem = (id: string) => {
 
     const item = menuItems.find(
       (menuItem) => menuItem.id === id
